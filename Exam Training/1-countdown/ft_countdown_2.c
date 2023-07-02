037a1bf80c955e57395e04e55f085c20758886b0
Yağmur Özerdem <138295010+fyozerdem@users.noreply.github.com>
2023-07-02 18:51:20 +0300

    Add files via upload

---
+++ Exam Training/1-countdown/ft_countdown_2.c
@@ -0,0 +1,6 @@
+#include <unistd.h>
+int main(void)
+{
+    write(1,"9876543210",10);
+    write(1,"\n",1);
+}
---
+++ Exam Training/1-maff_revalpha/maff_revalpha.c
@@ -0,0 +1,7 @@
+#include <unistd.h>
+
+int main(void)
+{
+   write(1, "zYxWvUtSrQpOnMlKjIhGfEdCbA", 26);
+   return 0;
+}
